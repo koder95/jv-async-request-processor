3a74b669513a37c1532cb6ea3e918efb77e2df3e
Kamil Jan Mularski <kammul@outlook.com>
2026-03-02 14:10:42 +0100

    Read and write lock

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -1,27 +1,42 @@
 package mate.academy;
 
-import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executor;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class AsyncRequestProcessor {
     private final Executor executor;
-    private final Map<String, UserData> cache = new ConcurrentHashMap<>();
+    private final ConcurrentMap<String, UserData> cache = new ConcurrentHashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
 
     public AsyncRequestProcessor(Executor executor) {
         this.executor = executor;
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        if (cache.containsKey(userId)) {
-            return CompletableFuture.completedFuture(cache.get(userId));
+        lock.readLock().lock();
+        try {
+            if (cache.containsKey(userId)) {
+                return CompletableFuture.completedFuture(cache.get(userId));
+            }
+        } finally {
+            lock.readLock().unlock();
         }
         CompletableFuture<UserData> future = CompletableFuture.supplyAsync(
                 () -> getUserData(userId),
                 executor
         );
-        future.thenAcceptAsync(userData -> cache.put(userId, userData), executor);
+        future.thenAcceptAsync(userData -> {
+            lock.writeLock().lock();
+            try {
+                cache.put(userId, userData);
+            } finally {
+                lock.writeLock().unlock();
+            }
+        }, executor);
         return future;
     }
 
